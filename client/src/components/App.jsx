56ba0ac55d0607d0f204e6ce1f974a287c53a66a
Miles Manning <mileslmanning@gmail.com>
2019-06-19 12:59:46 -0500

    Config complete - Current linter error with render statement? Next to be fixed

---
+++ client/src/components/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+export default class App extends React.component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    };
+
+    render() {
+      return (
+        <div>
+          <p>Hello World</p>
+        </div>
+      )
+    };
+  };
+};
